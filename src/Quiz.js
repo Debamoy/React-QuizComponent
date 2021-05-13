672284d05cd2a260cfab82eec1a18631104d0dfa
Debamoy Datta <datta.debamoy@gmail.com>
2021-05-13 14:50:42 +0530

    made changes upto instruction 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,2 +1,18 @@
 import React, { Component } from 'react';
-let quizData = require('./quiz_data.json')
+let quizData = require('./quiz_data.json');
+class Quiz extends Component{
+    constructor(props) {
+        super(props);
+        thios.state = {
+            quiz_position: 1
+        }
+    }
+    render() {
+        return (
+            <div>
+                Quiz
+            </div>
+        );
+    }
+}
+export default Quiz;
