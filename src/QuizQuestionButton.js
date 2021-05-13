073447169580fdf0d458b235c7d38d0443a380ec
Debamoy <datta.debamoy@gmail.com>
2021-05-13 18:02:15 +0530

    added QuizButton

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class QuizQuestionButton extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <li>
+                <button></button>
+            </li>
+        );
+    }
+}
+export default QuizQuestionButton;
